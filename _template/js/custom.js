874b6858cc3ede6aba8d5e2107515b66d316538f
tomaspotesil <tomas@potesil.com>
2022-04-03 15:26:30 +0200

    Index update

--- _template/js/custom.js
+++ _template/js/custom.js
@@ -1,3 +1,20 @@
-$("div.submenu").hover(function(){
-	$(this).parent().toggleClass('active');
-})
+$(document).ready(function() {
+
+	$("div.submenu").hover(function(){
+		$(this).parent().toggleClass('active');
+	})
+
+	AOS.init({
+		once: true
+	});
+
+	var loopVideo=document.getElementById("loop");
+
+      $('.video-container').waypoint(function() {                
+        loopVideo.play(); 
+        }, {
+          offset: '50%',
+          triggerOnce: true
+    });
+
+});

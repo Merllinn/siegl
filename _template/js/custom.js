b5b7661390c226ae702017b5fce00a5bec67d2e5
tomaspotesil <tomas@potesil.com>
2022-04-18 22:09:45 +0200

    Objednávka

--- _template/js/custom.js
+++ _template/js/custom.js
@@ -72,4 +72,20 @@ $(document).ready(function() {
     });
   });
 
+  $('.minus').click(function () {
+    var $input = $(this).parent().find('input#amount');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  
+  $('.plus').click(function () {
+    var $input = $(this).parent().find('input#amount');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+
 });
